30307ad96c4796022691e863bf21c8b28bdd6de6
J. Allen Burton <jburton6@my.westga.edu>
2020-02-28 14:45:31 -0500

    Composed RealEstateManager class constructor and methods.

---
+++ src/edu/westga/cs6312/files/model/RealEstateManager.java
@@ -0,0 +1,78 @@
+package edu.westga.cs6312.files.model;
+
+import java.util.ArrayList;
+
+/**
+ * This class will manage the currently listed RealEstate objects
+ * 
+ * @author J. Allen Burton
+ * @version Feb 28, 2020
+ *
+ */
+public class RealEstateManager {
+	private ArrayList<RealEstate> listedRealEstate;
+
+	/**
+	 * Constructor for RealEstateManager objects
+	 *
+	 * @precondition none
+	 *
+	 * @postcondition new RealEstateManager object with empty initialized ArrayList
+	 *                ready to take RealEstate objects
+	 */
+	public RealEstateManager() {
+		this.listedRealEstate = new ArrayList<RealEstate>();
+	}
+
+	/**
+	 * This method adds a RealEstate object to the current list
+	 * 
+	 * @param newProperty the property to be added to the list
+	 *
+	 * @precondition newProperty != null
+	 *
+	 * @postcondition RealEstate object added to listedRealEstate list in object
+	 */
+	public void addProperty(RealEstate newProperty) {
+		if (newProperty == null) {
+			throw new IllegalArgumentException("Invalid RealEstate object provided");
+		}
+		this.listedRealEstate.add(newProperty);
+	}
+
+	/**
+	 * Gets the full list of collected RealEstate objects held in this object
+	 * 
+	 * @return	ArrayList of RealEstate objects
+	 *
+	 * @precondition	none	
+	 *
+	 * @postcondition	no change to object
+	 */
+	public ArrayList<RealEstate> getProperties() {
+		return this.listedRealEstate;
+	}
+
+	/**
+	 * Returns a string representation of the object variables
+	 * 
+	 * @return	string representation of the object variables
+	 *
+	 * @precondition	none
+	 *
+	 * @postcondition	no change to object
+	 */
+	@Override
+	public String toString() {
+		String report = "Current Real Estate Listings:\n";
+		if (this.listedRealEstate.size() == 0) {
+			report += "\tThere are no current listings";
+		} else {
+			for (RealEstate currentProperty : this.listedRealEstate) {
+				report += currentProperty.toString() + "\n";
+			}
+		}
+		return report;
+	}
+
+}
